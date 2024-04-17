9feedff5593d3b35c0fd977ea3e784fdbcf2ab6e
Xiscor <xisco2002@gmail.com>
2024-04-17 19:23:23 +0200

    Header

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/proba', function () {
+    return view('proba');
+});
 
 Route::get('road-map/{id}/tasks', 'RoadMapController@showTasks')->name('roadmap.tasks');
